abe28205f0ffd59506e3da96ec02652e2b9f52ba
silanurs <sarii.silanur@gmail.com>
2023-05-24 21:45:07 +0300

    fetch data from a server

--- src/App.js
+++ src/App.js
@@ -2,10 +2,25 @@
 import './App.css';
 
 function App() {
+  async function getQuotes(){
+   try{
+    const response= await fetch('https://api.quotable.io/random');
+    const quoteData = await response.json()
+    console.log(quoteData)
+  } catch (error){
+    alert(error)
+  }
+  }
+  getQuotes()
+/* fetch('https://api.quotable.io/random').then(function(response){
+  return response.json();
+ }).then(function(response){
+ console.log(response.content);
+ }).catch(function(err){alert(err)})*/
   return (
     <div className="App">
       <div id="quote-box">
-        <p id="text">"How wonderful it is that nobody need wait a single moment before starting to improve the world.</p>
+        <p id="text">"</p>
         <p id="author">- Anne Frank</p>
         <div id="btnContainer">
         <button type="button">New quote</button>
@@ -15,7 +30,7 @@ function App() {
       <footer>
       <p>Copyright &copy; <script>
 document.write({new Date().getFullYear()})
-          </script> Silanurs<a href="https://github.com/silanurs" target="_blank" rel="noreferrer noopener"><i class="fa-brands fa-github"></i></a></p>
+          </script> Silanurs<a href="https://github.com/silanurs" target="_blank" rel="noreferrer noopener"><i className="fa-brands fa-github"></i></a></p>
           
 
       </footer>
