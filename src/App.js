7eb0d890206192259d8bd6f785672531c50d2cc4
silanurs <sarii.silanur@gmail.com>
2023-05-25 15:43:01 +0300

    finished project

--- src/App.js
+++ src/App.js
@@ -1,36 +1,54 @@
-
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [author, setAuthor] = useState("");
+  const [quote, setQuote] = useState("");
+  const [bgColor, setBgColor] = useState("")
+  useEffect(()=>{
   async function getQuotes(){
    try{
     const response= await fetch('https://api.quotable.io/random');
     const quoteData = await response.json()
-    console.log(quoteData)
+    console.log(quoteData.content)
+    setQuote(quoteData.content);
+    setAuthor(quoteData.author);
+    const randomR = Math.floor(Math.random() * 256)
+    const randomG = Math.floor(Math.random() * 256)
+    const randomB = Math.floor(Math.random() * 256)
+    setBgColor(`rgb(${randomR}, ${randomG}, ${randomB})`)
+    document.querySelector('body').style.backgroundColor=bgColor;
+    document.querySelector('button').style.backgroundColor=bgColor;
+    document.getElementById('text').style.color=bgColor;
+    document.getElementById('author').style.color=bgColor;
   } catch (error){
     alert(error)
   }
   }
-  getQuotes()
-/* fetch('https://api.quotable.io/random').then(function(response){
-  return response.json();
- }).then(function(response){
- console.log(response.content);
- }).catch(function(err){alert(err)})*/
+
+
+document.querySelector('button').addEventListener('click', getQuotes);
+window.addEventListener('load', getQuotes)
+
+return ()=>{
+  document.querySelector('button').removeEventListener('click', getQuotes);
+  window.removeEventListener('load', getQuotes)
+}
+}, [quote, bgColor])
+  
   return (
     <div className="App">
       <div id="quote-box">
-        <p id="text">"</p>
-        <p id="author">- Anne Frank</p>
+        <p id="text">"{quote}</p>
+        <p id="author">- {author}</p>
         <div id="btnContainer">
-        <button type="button">New quote</button>
+        <button type="button" >New quote</button>
         </div>
         
       </div>
       <footer>
-      <p>Copyright &copy; <script>
-document.write({new Date().getFullYear()})
-          </script> Silanurs<a href="https://github.com/silanurs" target="_blank" rel="noreferrer noopener"><i className="fa-brands fa-github"></i></a></p>
+      <p>Copyright &copy; 
+           Silanurs<a href="https://github.com/silanurs" target="_blank" rel="noreferrer noopener"><i className="fa-brands fa-github"></i></a></p>
           
 
       </footer>
